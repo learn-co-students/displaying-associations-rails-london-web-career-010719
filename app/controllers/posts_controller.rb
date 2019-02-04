ba9bea39f3c4071974d8671cbd9ceaab602ba145
studioshane <studioshane@gmail.com>
2019-02-04 13:52:49 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,12 @@
 class PostsController < ApplicationController
+
+	before_action :find_posts, only: [:index]
+	before_action :find_post, only: [:show, :edit, :update]
+
 	def index
-		@posts = Post.all
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def new
@@ -18,13 +20,23 @@ class PostsController < ApplicationController
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 		@categories = Category.all
 	end
 
 	def update
-		post = Post.find(params[:id])
-		post.update(params.require(:post))
-		redirect_to post_path(post)
+		@post.update(params.require(:post))
+		redirect_to post_path(@post)
 	end
+
+	private 
+
+	def find_post
+		@post = Post.find(params[:id])
+	end 
+
+	def find_posts
+		@posts = Post.all
+	end 
+
+
 end
